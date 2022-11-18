e3e84f806a6f02b6577cc2d79f5da82ae53bbafd
Omar Hamid <omarhamid664@gmail.com>
2022-11-17 22:09:28 -0500

    project createt

---
+++ blog/__init__.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+
+def create_app():
+  app = Flask(__name__, instance_relative_config=True)
+  app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'myblog.sqlite'),
+  )
+
+  try:
+    os.makedirs(app.instance_path)
+  except OSError:
+    pass
+  
+  @app.route("/")
+  def hello():
+    return "It's homepage of MyBlog"
+  
+  return app
