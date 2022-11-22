73d1aa92218c67926d1b99865fdc6ce8fd07131e
Omar Hamid <omarhamid664@gmail.com>
2022-11-22 16:22:50 -0500

    generates a unique name for each uploaded file

--- blog/__init__.py
+++ blog/__init__.py
@@ -3,8 +3,10 @@ from flask import Flask, g, flash, request, redirect, url_for, render_template
 from .auth import login_required
 from werkzeug.utils import secure_filename
 
+counter = 0
 
 def create_app():
+  
   app = Flask(__name__, instance_relative_config=True)
 
   UPLOAD_FOLDER = os.path.join(app.instance_path, 'images/')
@@ -45,8 +47,10 @@ def create_app():
     if request.method == "POST":
       file = request.files['file']
       if file and allowed_file(file.filename):
-          filename = secure_filename(file.filename)
-          file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        global counter
+        counter += 1
+        filename = str(counter) + secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     return render_template('create.html')
   
 
